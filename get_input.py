00fe79d954f8a7a9befb8db05c93d8ce244265d3
Jae-Yong Choi <jayong93@gmail.com>
2018-01-10 16:16:56 +0900

    dropout rate 2종류를 받도록 변경

--- get_input.py
+++ get_input.py
@@ -1,12 +1,19 @@
 import argparse
+import mmap
+import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--num_layers', type=int, help='number of RNN layers', default=3)
-parser.add_argument('--hidden_state', type=int, help='size of hidden state', default=1024)
-parser.add_argument('--seq_length', type=int, help='length of sequence', default=10)
-parser.add_argument('--learning_rate', type=float, help='set RNN learning rate', default=0.001)
-parser.add_argument('--dropout_rate', type=float, help='set RNN dropout rate', default=0.5)
-parser.add_argument('--batch_size', type=int, help='set RNN batch size', default=1)
-parser.add_argument('file_path', type=argparse.FileType('r'), help='path of input file', metavar='FILE_PATH')
-args = parser.parse_args()
-print(args)
+def get_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_layers', type=int, help='number of RNN layers', default=3)
+    parser.add_argument('--hidden_state', type=int, help='size of hidden state', default=1024)
+    parser.add_argument('--seq_length', type=int, help='length of sequence', default=10)
+    parser.add_argument('--learning_rate', type=float, help='set RNN learning rate', default=0.001)
+    parser.add_argument('--dropout_rate', type=float, nargs=2, help='set RNN dropout rate', default=[0.5, 0.5])
+    parser.add_argument('--batch_size', type=int, help='set RNN batch size', default=1)
+    parser.add_argument('file', type=argparse.FileType('r'), help='path of input file', metavar='FILE_PATH')
+    args = parser.parse_args()
+    print(args)
+
+    input_file = args.file
+
+get_arg()
