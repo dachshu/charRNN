e18a764b111bd7b3eb49bd6e5b1f8f0717b8ced4
Jae-Yong Choi <jayong93@gmail.com>
2018-01-10 16:20:43 +0900

    옵션 이름 변경

--- get_input.py
+++ get_input.py
@@ -8,7 +8,7 @@ def get_arg():
     parser.add_argument('--hidden_state', type=int, help='size of hidden state', default=1024)
     parser.add_argument('--seq_length', type=int, help='length of sequence', default=10)
     parser.add_argument('--learning_rate', type=float, help='set RNN learning rate', default=0.001)
-    parser.add_argument('--dropout_rate', type=float, nargs=2, help='set RNN dropout rate', default=[0.5, 0.5])
+    parser.add_argument('--dropout_rate', type=float, nargs=2, help='set RNN dropout rate', default=[0.5, 0.5], metavar=('INPUT_DROP_RATE', 'OUTPUT_DROP_RATE'))
     parser.add_argument('--batch_size', type=int, help='set RNN batch size', default=1)
     parser.add_argument('file', type=argparse.FileType('r'), help='path of input file', metavar='FILE_PATH')
     args = parser.parse_args()
